7f81abd2f060d8ed98499acd75646c59c1b490f4
Jay Lao <cjaylao447@gmail.com>
2026-01-30 23:08:51 +0800

    fix: correct syntax error in BODY_COPY string in card.ts

--- src/card.ts
+++ src/card.ts
@@ -2,7 +2,7 @@ import type { Year } from "./site";
 
 const BP_MEDIUM = 550;
 const BP_LARGE = 700;
-const BODY_COPY = `Jay Lao— I am dedicated and curious that enhances my skills to growth'
+const BODY_COPY = `Jay Lao— I am dedicated and curious that enhances my skills to growth`
 
 
 interface Props {
